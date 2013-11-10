df5d92e5fc1dd7dc69e0ce00f5d6daae86cec216
xdite <xuite.joke@gmail.com>
2013-11-11 00:19:25 +0800

    add post update destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,45 @@
 class PostsController < ApplicationController
 
 
- def new
-   @group = Group.find(params[:group_id])
-   @post = @group.posts.build
- end
+  def new
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.build
+  end
 
- def create
-   @group = Group.find(params[:group_id])
-   @post = @group.posts.new(post_params)
-   if @post.save
-     redirect_to group_path(@group)
-   else
-     render :new
-   end
- end 
+  def create
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.new(post_params)
+    if @post.save
+      redirect_to group_path(@group)
+    else
+      render :new
+    end
+  end 
 
- private
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
+    if @post.update(post_params)
+      redirect_to group_path(@group)
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
+    @post.destroy
+    redirect_to group_path(@group)
+  end
+
+  private
 
   def post_params
     params.require(:post).permit(:content)
