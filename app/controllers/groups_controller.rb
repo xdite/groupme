535baecaa7dd448a449f5958e401a4f5aadf765b
xdite <xuite.joke@gmail.com>
2013-11-10 23:59:09 +0800

    add group controllers

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,6 @@
+class GroupsController < ApplicationController
+
+  def index
+    @groups = Group.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Groupme::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+
+  resources :groups
+  
   root :to => 'high_voltage/pages#show', :id => 'welcome'
 end
