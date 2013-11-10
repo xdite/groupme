9b5ba8994a0221cd9e396b53999a0852337be511
xdite <xuite.joke@gmail.com>
2013-11-11 00:01:05 +0800

    Add group controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131110155549) do
+ActiveRecord::Schema.define(version: 20131110155748) do
+
+  create_table "groups", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                             default: "", null: false
