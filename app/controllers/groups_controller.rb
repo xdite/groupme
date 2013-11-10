573b98e891809662f3896dc90837e38e03af4058
xdite <xuite.joke@gmail.com>
2013-11-11 00:05:39 +0800

    Add group edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,5 +12,19 @@ class GroupsController < ApplicationController
     @group = Group.new
   end
 
+  def create
+    @group = Group.new(params[:group])
+    if @group.save redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+  
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
 
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,6 @@
 class Group < ActiveRecord::Base
+
+
+  validates :title, :presence => true
+  
 end
