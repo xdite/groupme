9d575cdb6272efdfa6cd68155cd6ae7e847d812d
sdlong <sdlong.jeng@gmail.com>
2013-12-31 05:34:11 +0800

    Update master

--- db/migrate/20131110155549_add_fb_id_to_user.rb
+++
@@ -1,7 +0,0 @@
-class AddFbIdToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :fb_id, :string, :limit => 20
-    add_column :users, :token, :string
-    add_column :users, :name, :string
-  end
-end
